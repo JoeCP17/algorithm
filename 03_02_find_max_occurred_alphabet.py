b92898bf3f432cdad454d207b9769c6d5c6deaaa
JoeCP17 <at365@naver.com>
2021-01-05 01:26:19 +0900

    week1 algorithm

---
+++ 03_02_find_max_occurred_alphabet.py
@@ -0,0 +1,23 @@
+def find_alphabet_occurrence_array(string):
+    alphabet_occurrence_array = [0] * 26
+
+    for alphabet in string:
+        if not alphabet.isalpha():
+            continue
+        arr_index = ord(alphabet) - ord("a")
+        alphabet_occurrence_array[arr_index] += 1
+
+    max_occurrence = 0
+    max_alphabet_index = 0
+    for index in range(len(alphabet_occurrence_array)):
+        # index 0 -> alphabet_occurrence 3
+        alphabet_occurrence = alphabet_occurrence_array[index]
+
+        if alphabet_occurrence > max_occurrence:
+            max_alphabet_index = index
+            max_occurrence = alphabet_occurrence
+    print(max_alphabet_index)
+    return chr(max_alphabet_index+ord("a"))
+
+
+print(find_alphabet_occurrence_array("hello my name is sparta"))
